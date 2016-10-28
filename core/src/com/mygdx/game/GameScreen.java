3b78f571726eac8bc4e2af67e42a27fff6d6bbca
skconan <supakit.kr@gmail.com>
2016-10-29 00:06:07 +0700

    add point follow mouse

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,23 +1,39 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
 	private SoldierGame soldierGame;
 	private Texture bgImg;
+	private Texture pointImg;
 	
 	public GameScreen(SoldierGame soldierGame){
 		this.soldierGame = soldierGame;
 		bgImg = new Texture("bg.png");
+		pointImg = new Texture("point.fw.png");
 	}
 	
 	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Vector2 pt = getPositionMouse();
         SpriteBatch batch = soldierGame.batch;
         batch.begin();
         batch.draw(bgImg, 0, 0);
+        batch.draw(pointImg, pt.x-60,SoldierGame.HEIGHT-pt.y-60);
         batch.end();
     }
 	
+	public Vector2 getPositionMouse() {
+		Vector2 position = new Vector2(0,0);
+		position.x = Gdx.input.getX();
+		position.y = Gdx.input.getY();
+		return position;		
+	}
 }
--- core/src/com/mygdx/game/SoldierGame.java
+++ core/src/com/mygdx/game/SoldierGame.java
@@ -2,6 +2,8 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
 public class SoldierGame extends Game {
+	public static final int HEIGHT = 385;
+	public static final int WIDTH = 800;
 	SpriteBatch batch;
 	
 	public void create () {
