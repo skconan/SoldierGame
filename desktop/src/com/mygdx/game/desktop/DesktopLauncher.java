0d6fa06f90f9311038dab37a12edeafb0db27856
skconan <tulconan@gmail.com>
2016-12-27 00:34:09 +0700

    add all

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package com.mygdx.game.desktop;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.SoldierGame;
@@ -8,7 +7,6 @@ import com.mygdx.game.SoldierGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.useGL30 = false;
 		config.height = SoldierGame.HEIGHT;
 		config.width = SoldierGame.WIDTH;
 		
